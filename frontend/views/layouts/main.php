09cdda51b52316d411c2d0decddeecc9f52fe292
jaybril <jaybril@163.com>
2015-12-02 00:53:16 +0800

    兼容性

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,6 +26,8 @@ if($model->logo){
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
