6c9a7d013abc78d809b15dd17f28db8f5ce95955
jaybril <jaybril@163.com>
2015-12-09 22:00:42 +0800

    文章编辑添加图片支持

--- backend/views/multimediaManger/addArticle.php
+++ backend/views/multimediaManger/addArticle.php
@@ -41,13 +41,17 @@
                             //定制菜单
                             'toolbars' => [
                                 [
-                                    'imagePopup','imageScaleEnabled','source', 'undo', 'redo', '|',
-                                    'fontsize',
-                                    'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'removeformat','autotypeset','justify','|',
-                                    'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|',
-                                    'forecolor', 'backcolor', '|',
-                                    'lineheight', '|',
-                                    'indent', '|'
+                                    'fullscreen', 'source', '|', 'undo', 'redo', '|',
+                                    'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+                                    'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+                                    'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+                                    'directionalityltr', 'directionalityrtl', 'indent', '|',
+                                    'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+                                    'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+                                    'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
+                                    'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
+                                    'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
+                                    'print', 'preview', 'searchreplace', 'help', 'drafts'
                                 ],
                             ]
                         ]
